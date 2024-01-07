e3da44247f561368bb4febccab91cf5e1d41e6d7
Nilaksha00 <nilakshasperera@gmail.com>
2024-01-07 11:16:04 +0000

    items adding and deleting from the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import store from "./store/store";
 
 export default function App() {
+  const [desc, setDesc] = useState("");
+
+  store.subscribe(() => {
+    console.log("CHANGED", store.getStore());
+  });
+
+  const AddBug = () => {
+    store.dispatch({
+      type: "ADD_BUG",
+      payload: {
+        description: desc,
+      },
+    });
+    // console.log(store.getState());
+  };
+
+  const DeleteBug = () => {
+    store.dispatch({
+      type: "REMOVE_BUG",
+      payload: {
+        id: 2,
+      },
+    });
+    // console.log(store.getState());
+  };
+
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit
+      <input type="text" onChange={(e) => setDesc(e.target.value)} />
+      <button onClick={AddBug}>Add</button>
+      <button onClick={DeleteBug}>Delete</button>
     </main>
-  )
+  );
 }
